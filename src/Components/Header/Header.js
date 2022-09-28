e282b42c2dcfc8d47ee38237cfbf091687218888
ahadhasanjion <niallhoran3176@gmail.com>
2022-09-29 00:36:04 +0600

    header

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className='company-name text-center m-7'>
+            <h1 className="font-bold text-4xl">Power Zone</h1>
+        </div>
+    );
+};
+
+export default Header;
