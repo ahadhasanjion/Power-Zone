010a19da9f64cfb8f56a74c8b395e93ebfac62f1
ahadhasanjion <niallhoran3176@gmail.com>
2022-09-29 20:03:35 +0600

    footer

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Cart.css';
 import images from'../../images/pexels-pixabay-260352.jpg';
 import { ToastContainer, toast } from 'react-toastify';
@@ -11,6 +11,12 @@ const Cart = (props) => {
    }
    const notify = () => toast("Wow so easy!");
 
+   const second = [10, 20, 30, 40, 50]
+   const [addbreak, SetAddBreakTime] = useState(0);
+   const handleClick = (y) => {
+    SetAddBreakTime(y)
+   }
+
     
     return (
         <div className="cart bg-gray-400">
@@ -21,16 +27,24 @@ const Cart = (props) => {
                     <p>Mullinger, Ireland</p>
                 </div>
             </div>
+            <div className='peroson-info'>
+                <div>
+                    
+                </div>
+
+            </div>
             <div className='my-6'>
                 <p className='px-3 mb-4'>Add A Break</p>
-                <div className='add-a-break flex'>
-                    <ul className='grid grid-cols-5'>
-                        <li>10s</li>
-                        <li>20s</li>
-                        <li>30s</li>
-                        <li>40s</li>
-                        <li>50s</li>
-                    </ul>
+                <div className='add-a-break flex gap-2 ml-3'>
+                    {
+                        second.map(t =><button onClick={() => handleClick(t)} key={t} className='btn btn-xl'>{t}<small>s</small></button>
+                        )
+                    }
+                    {/* <button onClick={() => handleClick(addbreak)} className='btn btn-xl'>10<small>s</small></button>
+                    <button onClick={() => handleClick(addbreak)} className='btn btn-xl'>20<small>s</small></button>
+                    <button onClick={() => handleClick(addbreak)} className='btn btn-xl'>30<small>s</small></button>
+                    <button onClick={() => handleClick(addbreak)} className='btn btn-xl'>40<small>s</small></button>
+                    <button onClick={() => handleClick(addbreak)} className='btn btn-xl'>50<small>s</small></button> */}
                 </div>
             </div>
             <div className='ed px-3'>
@@ -41,7 +55,7 @@ const Cart = (props) => {
                 </div>
                 <div className='flex justify-between p-2 bg-base-300 rounded mt-4 py-3'>
                     <p>Break Time</p>
-                    <p>seconds</p>
+                    <p>{addbreak}s</p>
                 </div>
             </div>
             <div onClick={notify} className='btn-completed'>
